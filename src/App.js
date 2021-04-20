222a1986d5137d65a963fadfee61b97f641aa89a
antoinemrn <antoinemarinier@gmail.com>
2021-04-20 11:42:45 +0200

    Basic Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import TaskList from './screen/TaskList';
+import {BrowserRouter , NavLink, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <TaskList/>
+      <BrowserRouter>
+        <div  className="body-container">
+          <div className="nav-container">
+            <NavLink to="/taskList" activeClassName="nav-link-active" className="nav-link">Task List</NavLink>
+            <NavLink to="/test" activeClassName="nav-link-active" className="nav-link">Task List</NavLink>
+            <NavLink to="/test2" activeClassName="nav-link-active" className="nav-link">Task List</NavLink>
+            <NavLink to="/test3" activeClassName="nav-link-active" className="nav-link">Task List</NavLink>
+            <NavLink to="/test5" activeClassName="nav-link-active" className="nav-link">Task List</NavLink>
+          </div>        
+          <div className="content">
+            <Route path="/taskList" component={TaskList}></Route>
+          </div>
+        </div>
+      </BrowserRouter >
+      
     </div>
   );
 }
